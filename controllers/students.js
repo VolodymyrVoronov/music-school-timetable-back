8de378b7b0d61a89378f0955924f0df264c4d977
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-08-06 20:46:58 +0200

    chore(controllers, models, routes): added getTeachersStudents, implemented filter in the students' controller

--- controllers/students.js
+++ controllers/students.js
@@ -15,3 +15,16 @@ export const addNewStudent = async (req, res) => {
     res.status(409).json({ message: error.message });
   }
 };
+
+export const getTeachersStudents = async (req, res) => {
+  const teachertId = req.userId;
+
+  try {
+    const students = await Student.find();
+    const filteredStudents = students.filter((student) => String(student.teacher) === String(teachertId));
+
+    res.status(200).json(filteredStudents);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
--- routes/students.js
+++ routes/students.js
@@ -1,11 +1,12 @@
 import express from "express";
 
-import { addNewStudent } from "./../controllers/students.js";
+import { addNewStudent, getTeachersStudents } from "./../controllers/students.js";
 
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
 router.post("/", auth, addNewStudent);
+router.get("/getStudents", auth, getTeachersStudents);
 
 export default router;
